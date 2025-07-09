ab1e3dc10ae2e3827359821470101c7f1bbed33d
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-07-08 21:17:52 -0300

    Inclusão das Annotations de validacao

--- src/main/java/com/rubenialima/picpay/controller/WalletController.java
+++ src/main/java/com/rubenialima/picpay/controller/WalletController.java
@@ -3,6 +3,7 @@ package com.rubenialima.picpay.controller;
 import com.rubenialima.picpay.controller.dto.CreateWalletDto;
 import com.rubenialima.picpay.entity.Wallet;
 import com.rubenialima.picpay.service.WalletService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,7 @@ public class WalletController {
     }
 
     @PostMapping("/wallets")
-    public ResponseEntity<Wallet> createWallet(@RequestBody CreateWalletDto dto){
+    public ResponseEntity<Wallet> createWallet(@RequestBody  @Valid  CreateWalletDto dto){
         var wallet = walletService.createWallet(dto);
 
         return ResponseEntity.ok(wallet);
