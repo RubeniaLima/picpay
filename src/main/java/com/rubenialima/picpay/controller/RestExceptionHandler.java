2b02e46d67773999e78eecdf71ec849760f5c5b7
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-07-11 22:05:58 -0300

    Implementando o metodo handleMethodArgumentNotValidException

--- src/main/java/com/rubenialima/picpay/controller/RestExceptionHandler.java
+++ src/main/java/com/rubenialima/picpay/controller/RestExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.rubenialima.picpay.controller;
 
 import com.rubenialima.picpay.exception.PicpayException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,4 +14,20 @@ public class RestExceptionHandler {
     public ProblemDetail handlePicPayException(PicpayException e){
         return e.toProblemDetail();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        var fieldErros = e.getFieldErrors()
+                .stream()
+                .map(f-> new InvalidParam( f.getField(), f.getDefaultMessage()))
+                .toList();
+
+        var pb = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+
+        pb.setTitle("Your request parameters didn´t validate.");
+        pb.setProperty("invalid-params", fieldErros);
+        return pb;
+    }
+
+    private record InvalidParam(String name, String reason){}
 }
