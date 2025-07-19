ec07dfc0679ae74070112c6bb57bb039a2c871b7
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-07-18 22:14:36 -0300

    Criação das Classes TransferDto e TransferRepository

--- src/main/java/com/rubenialima/picpay/controller/TransferController.java
+++ src/main/java/com/rubenialima/picpay/controller/TransferController.java
@@ -1,6 +1,12 @@
 package com.rubenialima.picpay.controller;
 
+import com.rubenialima.picpay.controller.dto.TransferDto;
+import com.rubenialima.picpay.entity.Transfer;
 import com.rubenialima.picpay.service.TransferService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +18,10 @@ public class TransferController {
         this.transferService = transferService;
     }
 
+    @PostMapping("/transfer")
+    public ResponseEntity<Transfer> transfer(@RequestBody @Valid TransferDto dto){
+           var resp = transferService.transfer(dto);
 
+           return ResponseEntity.ok(resp);
+    }
 }
--- src/main/java/com/rubenialima/picpay/service/TransferService.java
+++ src/main/java/com/rubenialima/picpay/service/TransferService.java
@@ -1,9 +1,31 @@
 package com.rubenialima.picpay.service;
 
+import com.rubenialima.picpay.controller.dto.TransferDto;
+import com.rubenialima.picpay.entity.Transfer;
+import com.rubenialima.picpay.repository.TransferRepository;
+import com.rubenialima.picpay.repository.WalletRepository;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
 @Service
 public class TransferService {
 
+    private final TransferRepository transferRepository;
+    private final AuthorizationService authorizationService;
+    private final NotificationService notificationService;
+    private final WalletRepository walletRepository;
+
+    public TransferService(TransferRepository transferRepository, AuthorizationService authorizationService, NotificationService notificationService, WalletRepository walletRepository) {
+        this.transferRepository = transferRepository;
+        this.authorizationService = authorizationService;
+        this.notificationService = notificationService;
+        this.walletRepository = walletRepository;
+    }
+
+
+    public Transfer  transfer( TransferDto dto) {
+
+        return null;
+    }
 }
